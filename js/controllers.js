4d68fdd1f66931adf767c335f3814392d98d70a3
Sergey Serzhan <sergey.serjan@apalon.com>
2016-10-07 11:53:26 +0300

    svoy slide

--- js/controllers.js
+++ js/controllers.js
@@ -45,15 +45,47 @@ wallpapersApp.controller('pageCtrl', ['$scope', '$http', '$location', function (
         for (i=0; i<$scope.images.length; i++){
             imageUrlArray.push($scope.images[i].imageUrl);
         }
+        console.log($scope.images.length);
         $scope.imageUrl = imageUrlArray;
-       /* var tt = angular.element(document.querySelector("#test"));
-        console.log(tt);*/
-        /*tt.remove();*/
         $scope.newImg = $scope.imageUrl[0];
+
+
+        /* contrl slide*/
+        var index = 0;
         $scope.setImg = function (imgt) {
+            index = $scope.imageUrl.indexOf(imgt);
+            console.log('previy-'+ index);
             $scope.mySwitch = true;
             $scope.newImg = imgt;
-            console.log($scope.newImg);
+        };
+        $(document).keydown(function(e) {
+            switch (e.which) {
+                case 37: // Left arrow
+                    console.log('left');
+                    $scope.prevSlide();
+                    break;
+                case 39: // Right arow
+                    console.log('right');
+                    $scope.nextSlide();
+                    break;
+                case 27: // Escape
+                    console.log('exit');
+                    $scope.closeSlide();
+                    break;
+            }
+        });
+        $scope.nextSlide = function (){
+            index++;
+            $scope.newImg = $scope.imageUrl[index == $scope.images.length ? index=$scope.images.length-1 : index];
+            console.log($scope.images.length+':'+index);
+        };
+        $scope.prevSlide = function (){
+            index--;
+            $scope.newImg = $scope.imageUrl[index==-1 ? index=0 : index];
+            console.log(index);
+        };
+        $scope.closeSlide = function(){
+            $scope.mySwitch = false;
         };
     });
 }]);
